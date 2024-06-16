b586f4c6e8540ae04d2cfa90038c07b82150ae35
stas <spolonsky@icloud.com>
2024-06-16 15:26:52 -0600

    initial commiit

--- README.md
+++ README.md
@@ -1,2 +1,49 @@
-# enzyme-reports
-Report on Enzyme vault performance
+# Report expenses for an Enzyme vault
+
+## Setup project
+
+I tested the code only on Ubuntu 22.04.
+
+### Start Python virtual environment
+
+```sh
+python3 -m venv env
+source env/bin/activate
+```
+
+### Install dependencies
+
+```sh
+pip install -r requirements.txt
+```
+
+### Add API keys to your `.env` file
+
+```txt
+MORALIS_API_KEY=...
+COINGECKO_API_KEY=...
+```
+
+You can get free API keys from both API providers
+
+## Query Enzyme API to download transactions as JSON
+
+At present, I use Buf Studio to [access the API](https://buf.build/studio/avantgardefinance/enzyme/enzyme.enzyme.v1.EnzymeService):
+
+1. Add the target URL: [https://api.enzyme.finance/](https://api.enzyme.finance/)
+
+2. In `Body`, add vault `address`
+
+3. In `Headers` add authorization header (the site provide free API tokens):
+    - `Key`: `Authorization`
+    - `Value`: `Bearer API-token`
+
+4. In Method field, first select repository `avantgardefinance/enzyme`, next `GetVaultActivities`
+
+5. Send the request, save the response JSON to `vault-activity.json` in project root directory
+
+## Run Python script to generate report
+
+```sh
+python report.py
+```
---
+++ report.py
@@ -0,0 +1,101 @@
+import shelve
+from moralis import evm_api
+import json
+from dotenv import load_dotenv
+import os
+import time
+from datetime import datetime
+import requests
+from prettytable import PrettyTable
+
+def normalize_date(datetime_str):
+    datetime_obj = datetime.strptime(datetime_str, '%Y-%m-%dT%H:%M:%S.%fZ')
+    formatted_date = datetime_obj.strftime('%d-%m-%Y')
+    return formatted_date
+
+class EthereumHistoricPrice:
+    def __init__(self, currency="usd"):
+        self.db = shelve.open("prices.db", writeback=True) 
+        self.currency = currency
+        self.api_key = os.getenv('COINGECKO_API_KEY')
+
+    def __del__(self):
+        # print("Closing Prices database")
+        self.db.close()
+
+    def quote(self, date):
+        if date in self.db:
+            # print(f"Retrieving {date} from cache")
+            pass
+        else:
+            url = f"https://api.coingecko.com/api/v3/coins/ethereum/history?date={date}&localization=false"
+            headers = {"accept": "application/json","x-cg-api-key": self.api_key}
+            response = requests.get(url, headers=headers)
+            if response.status_code == 200:
+                self.db[date] = response.json()
+                self.db.sync()
+                print(f"Retrieved {date} from API")
+            else:
+                print(f"Failed to retrieve {date} from API")
+                return None
+        return self.db[date]["market_data"]["current_price"][self.currency]
+    
+class TradesData:
+    def __init__(self):
+        self.db = shelve.open("trades.db") 
+        self.api_key = os.getenv('MORALIS_API_KEY')
+
+    def __del__(self):
+        # print("Closing Trades database")
+        self.db.close()
+
+    def qet(self, transaction_hash):
+        if transaction_hash in self.db:
+            # print('Transaction already exists')
+            pass
+        else:
+            # print('Transaction does not exist, adding it to the shelve')
+            params = {"chain": "eth", "transaction_hash": transaction_hash}
+            result = evm_api.transaction.get_transaction(api_key=self.api_key,params=params)
+            self.db[transaction_hash] = result
+            self.db.sync()
+            # I use free API, so I need to wait in case throughput limits are reached
+            time.sleep(1)
+        return self.db[transaction_hash]
+        
+load_dotenv()
+
+# Initialize locally cached obbjects
+price_usd = EthereumHistoricPrice("usd")
+trade = TradesData()
+
+with open('vault-activity.json', 'r') as file:
+    data = json.load(file)
+
+def get_part_before_slash(s):
+    return s.split('/')[0]
+
+
+
+# Create a table object
+table = PrettyTable()
+table.field_names = ["Date", "Fee (USD)"]
+
+def trade_fee(transaction_hash,table):
+    t = trade.qet(transaction_hash)
+    fee_eth = float(t["transaction_fee"])
+    block_date = normalize_date(t["block_timestamp"])
+    eth_price = price_usd.quote(block_date)
+    fee_usd = fee_eth * eth_price
+    print(f'Date  {block_date} Fee (USD): {fee_usd}' )
+    table.add_row([block_date, "{:.2f}".format(fee_usd)])
+    
+for activity in data['vaultActivities']:
+    if 'trade' in activity:
+        transaction_hash = get_part_before_slash(activity['trade']['id'])
+        print(f'Trade {transaction_hash}')
+        trade_fee(transaction_hash, table)
+
+print(table)
+
+    
