d75ccf638f1effc62e090ce60bd79af3799b2005
stas <spolonsky@icloud.com>
2024-06-30 12:40:41 -0600

    coid data to separate file

--- report-asset-allocation.py
+++ report-asset-allocation.py
@@ -1,12 +1,13 @@
-# Report on Asset Allocation and how it compares with target
-
-# Enzyme API documentation: https://enzymefinance.github.io/sdk/api/overview
+# Report on Asset Allocation and how it compares with the targets
+# Use Enzyme API documentation: https://enzymefinance.github.io/sdk/api/overview
 
 import requests
 import json
 from dotenv import load_dotenv
 import os
 import pandas as pd
+from matplotlib.backends.backend_pdf import PdfPages
+import matplotlib.pyplot as plt
 
 pd.set_option('display.float_format', '{:.2f}'.format)
 load_dotenv()
@@ -14,7 +15,7 @@ load_dotenv()
 # URL for the request
 url = "https://api.enzyme.finance/enzyme.enzyme.v1.EnzymeService/GetVaultPortfolio"
 
-# Headers to be sent with the request
+# Request headers
 ENZYME_API_KEY=os.getenv('ENZYME_API_KEY')
 headers = {
     "Content-Type": "application/json",
@@ -22,7 +23,7 @@ headers = {
     "Connect-Protocol-Version": "1"
 }
 
-# Data to be sent in JSON format
+# Request data to be sent in JSON format
 address = os.getenv('ENZYME_VAULT_ADDRESS')
 data = {
   "deployment": "DEPLOYMENT_UNSPECIFIED",
@@ -34,110 +35,72 @@ data = {
 response = requests.post(url, headers=headers, data=json.dumps(data))
 
 # convert json to pandas dataframe
-
-
 assets = pd.DataFrame(response.json()["assets"])
 
 
+
+with open('coin_data.json', 'r') as f:
+    coin_data = json.load(f)
+
 # Coin Name - specific coin name 
 # Asset Name - few coins can contribute to the same asset (e.g. WBTC and renBTC both contribute to BTC)
-coin_data = {
-     
-    "0x2260fac5e5542a773aa44fbcfedf7c193bc2c599": {
-          "Coin Name": "WBTC",
-         "Asset Name": "BTC",
-        "Asset Class": "Native Coins"
-    },
-
-    "0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2": {
-          "Coin Name": "WETH",
-         "Asset Name": "ETH",
-        "Asset Class": "Native Coins"
-    },
-
-    "0xae7ab96520de3a18e5e111b5eaab095312d7fe84": { 
-          "Coin Name": "stETH",
-         "Asset Name": "ETH",
-        "Asset Class": "Native Coins"
-    },
-
-    "0xd31a59c85ae9d8edefec411d448f90841571b89c" : {
-          "Coin Name": "WSOL",
-         "Asset Name": "SOL",
-        "Asset Class": "Native Coins"
-    },
-
-    "0xdac17f958d2ee523a2206206994597c13d831ec7": {
-          "Coin Name": "USDT",
-         "Asset Name": "USDT",
-        "Asset Class": "Stable Coins"
-    },
-
-    "0x23878914efe38d27c4d67ab83ed1b93a74d4086a": {
-            "Coin Name": "aEthUSDT", 
-             "Asset Name": "USDT",
-            "Asset Class": "Stable Coins"
-    },
-
-    "0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48": {
-          "Coin Name": "USDC",
-         "Asset Name": "USDC",
-        "Asset Class": "Stable Coins"
-    },
-
-    "0x6b175474e89094c44da98b954eedeac495271d0f": {
-          "Coin Name": "DAI",
-         "Asset Name": "DAI",
-        "Asset Class": "Stable Coins"
-    },
-
-    "0x018008bfb33d285247a21d44e50697654f754e63": {
-          "Coin Name": "aEthDAI",
-         "Asset Name": "DAI",
-        "Asset Class": "Stable Coins"
-    },
-
-    "0xec67005c4e498ec7f55e092bd1d35cbc47c91892": {
-          "Coin Name": "MLN",
-         "Asset Name": "MLN",
-        "Asset Class": "Internal"
-    },
-}
-
-# add a new column to the dataframe by mapping address to the coin_data dictionary's Coin Name
+# Use coin data to modify the table 
 assets["Coin Name"] = assets["address"].map(lambda x: coin_data[x]["Coin Name"])
-# add a new column to the dataframe by mapping address to the coin_data dictionary's Asset Name
 assets["Asset Name"] = assets["address"].map(lambda x: coin_data[x]["Asset Name"])
-# add a new column to the dataframe by mapping address to the coin_data dictionary's Asset Class
 assets["Asset Class"] = assets["address"].map(lambda x: coin_data[x]["Asset Class"])
-# delete the address column
 del assets["address"]
 
-# pretty print the dataframe
-#print(assets)
-
-# calculate the total value of the assets
 total_value = assets["value"].sum()
 print(f"\nTotal Value: {total_value:.2f}")
 
 # calculate the total value of the assets by asset class
+# assets_copy = assets.copy(deep=True)
 assets_by_class = assets.groupby('Asset Class').sum() 
 # keep only these columns
 assets_by_class.drop(assets_by_class.columns.difference(['value', 'Asset Class']), axis=1, inplace=True)
-# add a new column to the dataframe with the percentage of the total value
 assets_by_class["Percentage"] = assets_by_class["value"] / total_value * 100
+assets_by_class["Reference"] = 0
+assets_by_class.loc["Native Coins", "Reference"] = 70
+assets_by_class.loc["Stable Coins", "Reference"] = 30
+
+total_by_class = {}
+total_by_class["Native Coins"] = assets_by_class["value"]["Native Coins"]
+total_by_class["Stable Coins"] = assets_by_class["value"]["Stable Coins"]
+
 print(f'\nAssets by class:')
-print(assets_by_class)
 
 
-def report_inclass_allocations(index_csv, assets, class_name='Native Coins'):
+
+def table_to_pdf(df, filename):
+    with PdfPages(filename) as pdf:
+        fig, ax =plt.subplots(figsize=(8, 3))  # Adjust as necessary
+        ax.axis('tight')
+        ax.axis('off')  # Hide the axes
+        the_table = ax.table(cellText=df.values, colLabels=df.columns, loc='center', cellLoc='center')
+        # Optional: Configure table properties
+        the_table.auto_set_font_size(False)
+        the_table.set_fontsize(12)
+        the_table.scale(1.2, 1.2)  # Adjust table size
+        pdf.savefig(fig, bbox_inches='tight')
+        plt.close()
+
+assets_by_class = assets_by_class.round(2) 
+# sort the values by the 'Percentage' column
+assets_by_class = assets_by_class.sort_values(by='Reference', ascending=False)
+
+c = assets_by_class.copy(deep=True)
+c = c.reset_index()
+print(c)
+table_to_pdf(c, 'report/assets_by_class.pdf')
+
+def report_inclass_allocations(index_csv, assets, class_name, total):
     print(f'\nInclass allocations for {class_name}:')
     # keep rows with Asset Class = class_name
     coins = assets[assets["Asset Class"] == class_name]
     # aggregate the values by Asset Name
     inclass_assets = coins.groupby('Asset Name').sum()
     # add a new column to the dataframe with the percentage of the total value
-    inclass_assets["Percentage"] = inclass_assets["value"] / assets_by_class["value"][class_name] * 100
+    inclass_assets["Percentage"] = inclass_assets["value"] / total * 100
     # keep only these columns
     inclass_assets.drop(inclass_assets.columns.difference(['Asset Name', 'value', 'Percentage']), axis=1, inplace=True)
     # manual download https://www.coindesk.com/indices
@@ -147,16 +110,8 @@ def report_inclass_allocations(index_csv, assets, class_name='Native Coins'):
     # Add Reference column
     inclass_assets = inclass_assets.merge(index[['Asset Name', 'Weight']], on='Asset Name', how='left')
     inclass_assets.rename(columns={'Weight': 'Reference'}, inplace=True)
-    # in-place sort by Percentage
     inclass_assets.sort_values(by='Percentage', ascending=False, inplace=True)
-    # inclass_assets.style.format({
-    #     'value': '{:.2f}',
-    #     'Percentage': '{:.2f}',
-    #     'Reference': '{:.2f}'
-    # })
-    
-    # df['A'] = df['A'].map('{:.2f}'.format)
     print(inclass_assets.to_string(index=False))
 
-report_inclass_allocations('input/Constituents - CoinDesk Large Cap Select Index.csv', assets, 'Native Coins')
-report_inclass_allocations('input/Constituents - CoinDesk Stablecoin Index.csv', assets, 'Stable Coins')
+report_inclass_allocations('input/Constituents - CoinDesk Large Cap Select Index.csv', assets, 'Native Coins', assets_by_class["value"]["Native Coins"])
+report_inclass_allocations('input/Constituents - CoinDesk Stablecoin Index.csv', assets, 'Stable Coins', assets_by_class["value"]["Stable Coins"])
